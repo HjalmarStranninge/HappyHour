46bcbfa0a0f706a7578ea96f43fae159fe9f133b
Hjalmar Stranninge <Hjalmar.stranninge@outlook.com>
2023-12-20 15:47:29 +0100

    Add project files.

---
+++ HappyHour/DrinksWrapper.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace HappyHour
+{
+    internal class DrinksWrapper
+    {
+        [JsonPropertyName("drinks")]
+        public List<Drink> Drinks { get; set; }
+    }
+
+    internal class Drink
+    {
+        [JsonPropertyName("strDrink")]
+        public string StrDrink { get; set; }
+        [JsonPropertyName("strInstructions")]
+        public string StrInstructions { get; set; }
+
+        [JsonPropertyName("strIngredient1")]
+        public string StrIngredient1 { get; set; }
+        [JsonPropertyName("strIngredient2")]
+        public string StrIngredient2 { get; set; }
+        [JsonPropertyName("strIngredient3")]
+        public string StrIngredient3 { get; set; }
+        [JsonPropertyName("strIngredient4")]
+        public string StrIngredient4 { get; set; }
+        [JsonPropertyName("strIngredient5")]
+        public string StrIngredient5 { get; set; }
+        [JsonPropertyName("strIngredient6")]
+        public string StrIngredient6 { get; set; }
+        [JsonPropertyName("strIngredient7")]
+        public string StrIngredient7 { get; set; }
+        [JsonPropertyName("strIngredient8")]
+        public string StrIngredient8 { get; set; }
+        [JsonPropertyName("strIngredient9")]
+        public string StrIngredient9 { get; set; }
+        [JsonPropertyName("strIngredient10")]
+        public string StrIngredient10 { get; set; }
+        [JsonPropertyName("strIngredient11")]
+        public string StrIngredient11 { get; set; }
+        [JsonPropertyName("strIngredient12")]
+        public string StrIngredient12 { get; set; }
+        [JsonPropertyName("strIngredient13")]
+        public string StrIngredient13 { get; set; }
+        [JsonPropertyName("strIngredient14")]
+        public string StrIngredient14 { get; set; }
+        [JsonPropertyName("strIngredient15")]
+        public string StrIngredient15 { get; set; }
+
+        // Create a list to store ingredients
+        public List<string> Ingredients
+        {
+            get
+            {
+                return new List<string>
+            {
+                StrIngredient1,
+                StrIngredient2,
+                StrIngredient3,
+                StrIngredient4,
+                StrIngredient5,
+                StrIngredient6,
+                StrIngredient7,
+                StrIngredient8,
+                StrIngredient9,
+                StrIngredient10,
+                StrIngredient11,
+                StrIngredient12,
+                StrIngredient13,
+                StrIngredient14,
+                StrIngredient15
+            };
+            }
+        }
+    }
+}
---
+++ HappyHour/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Text.Json;
+
+namespace HappyHour
+{
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            string header = "  _    _                           _    _                  \r\n | |  | |                         | |  | |                 \r\n | |__| | __ _ _ __  _ __  _   _  | |__| | ___  _   _ _ __ \r\n |  __  |/ _` | '_ \\| '_ \\| | | | |  __  |/ _ \\| | | | '__|\r\n | |  | | (_| | |_) | |_) | |_| | | |  | | (_) | |_| | |   \r\n |_|  |_|\\__,_| .__/| .__/ \\__, | |_|  |_|\\___/ \\__,_|_|   \r\n              | |   | |     __/ |                          \r\n              |_|   |_|    |___/    \n"
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(header);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Press Enter to generate a cocktail!");
+            Console.CursorVisible = false;
+
+            while (true)
+            {               
+                ConsoleKeyInfo key = Console.ReadKey();
+
+                if ( key.Key == ConsoleKey.Enter)
+                {
+                    using (HttpClient client = new HttpClient())
+                    {
+
+                        HttpResponseMessage response =
+                            await client.GetAsync("https://www.thecocktaildb.com/api/json/v1/1/random.php");
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            string jsonResponse = await response.Content.ReadAsStringAsync();
+
+                            DrinksWrapper drink = JsonSerializer.Deserialize<DrinksWrapper>(jsonResponse);
+
+                            Console.Clear();
+                            Console.ForegroundColor = ConsoleColor.Magenta;
+                            await Console.Out.WriteLineAsync(header);
+                            Console.ForegroundColor = ConsoleColor.White;
+
+                            Console.SetCursorPosition(0, 10);
+
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            await Console.Out.WriteLineAsync($"{drink.Drinks[0].StrDrink}\n");
+                            Console.ForegroundColor = ConsoleColor.White;
+
+                            foreach(var ingredient in drink.Drinks[0].Ingredients)
+                            {
+                                if(ingredient != null)
+                                {
+                                    await Console.Out.WriteLineAsync(ingredient);
+                                }                               
+                            }
+
+                            await Console.Out.WriteLineAsync();
+
+                            await Console.Out.WriteLineAsync($"{drink.Drinks[0].StrInstructions}\n");
+
+                            await Console.Out.WriteLineAsync("Press Enter to generate a new cocktail");
+
+                            Console.SetCursorPosition(0, 0);
+                        }
+                        else
+                        {
+                            await Console.Out.WriteLineAsync("Fail");
+                        }
+
+                    }
+                }
+                
+            }
+            
+        }
+    }
+}
